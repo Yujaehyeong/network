7803e6941abec6865cdcd1eade94d566688e95f2
CJ <cjkim10@outlook.com>
2019-04-26 16:48:11 +0900

    modify nslookup.java

--- src/util/NsLookup.java
+++ src/util/NsLookup.java
@@ -2,29 +2,60 @@ package util;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class NsLookup {
-	
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		String url = "";
+
+	private String url;
+	private ArrayList<String> inetAddressList;
+
+	public NsLookup() {
+		inetAddressList = new ArrayList<String>();
+	}
+
+	public void setHostAddress(String url) {
+		InetAddress[] inetAddresses = null;
 		try {
-			while (true) {
-				System.out.print("> ");
-				url = sc.next();
-				if(url.equals("exit")) break;
-				InetAddress[] inetAddresses = InetAddress.getAllByName(url);
-				for (InetAddress i : inetAddresses) {
-					System.out.println(url + " : " + i.getHostAddress());
-				}
+			inetAddresses = InetAddress.getAllByName(url);
+			this.url = url;
+
+			for (InetAddress i : inetAddresses) {
+				inetAddressList.add(i.getHostAddress());
 			}
 
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		}
-
 	}
 
+	public boolean isExit(String url) {
+		boolean result = true;
+		if (!url.equals("exit")) {
+			setHostAddress(url);
+			result = false;
+		}
+		return result;
+	}
+
+	public void printHostAddress() {
+		if (inetAddressList.size() != 0) {
+			for (String hostName : inetAddressList) {
+				System.out.println(this.url + " : " + hostName);
+			}
+		}
+		inetAddressList.clear();
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		NsLookup nslookup = new NsLookup();
+		while (true) {
+			System.out.print("> ");
+			String url = sc.next();
+			if (nslookup.isExit(url))
+				break;
+			nslookup.printHostAddress();
+		}
+	}
 }
